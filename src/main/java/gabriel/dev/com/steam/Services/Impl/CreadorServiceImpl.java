7d5d91d5b88d42e227dd73be7f8d927b8b4a23c4
Scipioni-Gabriel-405262 <gabriel.scipioni21@gmail.com>
2024-08-03 00:58:13 -0300

    r

--- src/main/java/gabriel/dev/com/steam/Services/Impl/CreadorServiceImpl.java
+++ src/main/java/gabriel/dev/com/steam/Services/Impl/CreadorServiceImpl.java
@@ -1,14 +1,19 @@
 package gabriel.dev.com.steam.Services.Impl;
 
 
+import gabriel.dev.com.steam.Entities.CreadorEntity;
 import gabriel.dev.com.steam.Models.Creador;
 import gabriel.dev.com.steam.Repositories.CreadorRepository;
 import gabriel.dev.com.steam.Services.CreadorService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CreadorServiceImpl implements CreadorService {
@@ -19,14 +24,31 @@ public class CreadorServiceImpl implements CreadorService {
     private ModelMapper modelMapper;
 
     public Creador GetCreadoresByName (String nombre){
-        //todo traer solo a un creador por el nombre (String) si es que existe, si no devolver un null
-        return null;
+        // traer solo a un creador por el nombre (String) si es que existe, si no devolver un null
+        Optional<CreadorEntity> optionalCreador = creadorRepository.findByNombre(nombre);
+        if (optionalCreador.isEmpty()){
+            return null;
+        }
+        return modelMapper.map(optionalCreador.get(),Creador.class);
     }
-    public void CrearCreador (String nombre){
-        //todo crear a un creador de videojuegos a partir del nombre, asegurarse de que no exista usando el metodo existeCreador(String), en tal caso devolver un error de badRequest
+    public ResponseEntity<String> crearCreador(String nombre) {
+        // Verificar si el creador ya existe
+        if (existeCreador(nombre)) {
+            // Devolver una respuesta de BadRequest si el creador ya existe
+            return new ResponseEntity<>("El creador ya existe", HttpStatus.BAD_REQUEST);
+        }
+        // Crear una nueva entidad Creador
+        CreadorEntity creadorEntity = new CreadorEntity();
+        creadorEntity.setNombre(nombre);
+
+        // Guardar la entidad en la base de datos
+        creadorRepository.save(creadorEntity);
+
+        // Devolver una respuesta exitosa
+        return new ResponseEntity<>("Creador creado exitosamente", HttpStatus.CREATED);
     }
     private boolean existeCreador(String nombre){
-        //todo este metodo debe validar si un creador existe, true en el caso de que si, false en el caso de que no
-        return false;
+        //este metodo debe validar si un creador existe, true en el caso de que si, false en el caso de que no
+        return creadorRepository.existsByNombre(nombre);
     }
 }
