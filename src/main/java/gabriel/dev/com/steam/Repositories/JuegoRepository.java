19ba9d033bd91468703f424970db231c24e7ac57
Scipioni-Gabriel-405262 <gabriel.scipioni21@gmail.com>
2024-08-03 02:09:56 -0300

    resolution from juegos Repository and Service

--- src/main/java/gabriel/dev/com/steam/Repositories/JuegoRepository.java
+++ src/main/java/gabriel/dev/com/steam/Repositories/JuegoRepository.java
@@ -1,6 +1,7 @@
 package gabriel.dev.com.steam.Repositories;
 
 import gabriel.dev.com.steam.Entities.JuegoEntity;
+import gabriel.dev.com.steam.Models.Enums.Genero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +14,14 @@ public interface JuegoRepository extends JpaRepository<JuegoEntity, Long> {
     @Query("SELECT j FROM JuegoEntity j WHERE j.creador.nombre = :nombreCreador")
     List<JuegoEntity> findJuegosByCreadorNombre(@Param("nombreCreador") String nombreCreador);
 
+    List<JuegoEntity> findByGenero (Genero genero);
+
+    @Query("select j from JuegoEntity j where j.rating < :ratingMax and j.rating > :ratingMin")
+    List<JuegoEntity> findByRating (@Param("ratingMax") Integer ratingMax, @Param("ratingMin") Integer ratingMin);
+
+    @Query("select j from JuegoEntity j where j.precio < :precioMax and j.precio > :precioMin")
+    List<JuegoEntity> findByPrecio(@Param("precioMax") float precioMax, @Param("precioMin") float precioMin);
+
+    @Query("SELECT j FROM JuegoEntity j WHERE LOWER(j.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))")
+    List<JuegoEntity> findByNombre(@Param("nombre") String nombre);
 }
--- src/main/java/gabriel/dev/com/steam/Services/Impl/JuegoServiceImpl.java
+++ src/main/java/gabriel/dev/com/steam/Services/Impl/JuegoServiceImpl.java
@@ -1,6 +1,8 @@
 package gabriel.dev.com.steam.Services.Impl;
 
 
+import gabriel.dev.com.steam.Entities.CreadorEntity;
+import gabriel.dev.com.steam.Entities.JuegoEntity;
 import gabriel.dev.com.steam.Models.Enums.Genero;
 import gabriel.dev.com.steam.Models.Juego;
 import gabriel.dev.com.steam.Repositories.JuegoRepository;
@@ -9,6 +11,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,37 +26,121 @@ public class JuegoServiceImpl implements JuegoService {
 
     public List<Juego> busacarJuegosDeAutor(String nombreCreador){
         //se debera devolver los juegos asociados a un creador pasado por parametro, usar juegoRepository.findJuegosByCreadorNombre(nombreCreador)
-
-        return null;
+        List<JuegoEntity> juegosEntity =juegoRepository.findJuegosByCreadorNombre(nombreCreador);
+        if (juegosEntity.isEmpty()){
+            return null;
+        }
+        List<Juego> juegos = new ArrayList<>();
+        for (JuegoEntity j : juegosEntity){
+            juegos.add(modelMapper.map(j , Juego.class));
+        }
+        return juegos;
     }
+
     public void crearJuego(Genero genero, float precio, String creador, String nombre, String release){
-        //todo crear un juego con los parametros pasados, validar que el creador exista, y setearle un rating de cero
+        //crear un juego con los parametros pasados, validar que el creador exista, y setearle un rating de cero
+        JuegoEntity juego = new JuegoEntity();
+        juego.setGenero(genero);
+        juego.setPrecio(precio);
+        juego.setCreador(modelMapper.map(creador, CreadorEntity.class));
+        juego.setNombre(nombre);
+        juego.setRelease(release);
+        juego.setRating(0);
+        juegoRepository.save(juego);
     }
 
-    public List<Juego> BuscarJuegosPorFiltros(Genero genero, Integer ratingMax, Integer ratingMin, float precioMax, float precioMin, String nombre){
-        //todo devolver una lista de juegos en base a los filtros que no sean nulls, es decir si llega un genero de terror de precio max 10 y min 0 (siendo los demas parametros nulls) traer una lista (sin repetidos) de los juegos que tengan estas caracteristicas. En caso de pasarse el nombre no es necesario que este sea completamente acertado al nombre del juego (si se ingresa "fi" se devolvera los juegos del FIfa, FInal fantacy, hi-FI, free FIre, etc)
-        return null;
+    public List<Juego> BuscarJuegosPorFiltros(Genero genero, Integer ratingMax, Integer ratingMin, float precioMax, float precioMin, String nombre) {
+        // Lista suprema que contendrá los juegos que coincidan con los filtros
+        List<Juego> listaJuegosSuprema = new ArrayList<>();
+
+        // Aplicar filtro de género si se proporciona
+        if (genero != null) {
+            listaJuegosSuprema.addAll(buscarJuegoDeGenero(genero));
+        }
+
+        // Aplicar filtro de rating si se proporcionan ambos límites
+        if (ratingMax != null && ratingMin != null) {
+            List<Juego> juegosRating = buscarJuegoDeRating(ratingMax, ratingMin);
+            if (listaJuegosSuprema.isEmpty()) {
+                listaJuegosSuprema.addAll(juegosRating);
+            } else {
+                listaJuegosSuprema.retainAll(juegosRating);
+            }
+        }
+
+        // Aplicar filtro de precio si se proporcionan ambos límites
+        if (precioMax != 0 && precioMin != 0) {
+            List<Juego> juegosPrecio = buscarJuegoDePrecio(precioMax, precioMin);
+            if (listaJuegosSuprema.isEmpty()) {
+                listaJuegosSuprema.addAll(juegosPrecio);
+            } else {
+                listaJuegosSuprema.retainAll(juegosPrecio);
+            }
+        }
+
+        // Aplicar filtro de nombre si se proporciona
+        if (nombre != null) {
+            List<Juego> juegosNombre = buscarJuegoDeNombre(nombre);
+            if (listaJuegosSuprema.isEmpty()) {
+                listaJuegosSuprema.addAll(juegosNombre);
+            } else {
+                listaJuegosSuprema.retainAll(juegosNombre);
+            }
+        }
+
+        // Si no se proporciona ningún filtro, devolver todos los juegos
+        if (genero == null && ratingMax == null && ratingMin == null && precioMax == 0 && precioMin == 0 && nombre == null) {
+            return todosLosJuegos();
+        }
+
+        return new ArrayList<>(listaJuegosSuprema);
     }
+
 
     private List<Juego> buscarJuegoDeGenero(Genero genero){
-        //todo devolver una lista de juegos de un genero especifico
-        return null;
+        // devolver una lista de juegos de un genero especifico
+        List<JuegoEntity> juegosEntity = juegoRepository.findByGenero(genero);
+        List<Juego> juegos = new ArrayList<>();
+        for (JuegoEntity j : juegosEntity){
+            juegos.add(modelMapper.map(j,Juego.class));
+        }
+        return juegos;
     }
     private List<Juego> buscarJuegoDeRating(Integer RatingMax, Integer RatingMin){
-        //todo devolver una lista de juegos de un rating entre valores especificos
-        return null;
+        //devolver una lista de juegos de un rating entre valores especificos
+        List<JuegoEntity> juegosEntity = juegoRepository.findByRating(RatingMax,RatingMin);
+        List<Juego> juegos = new ArrayList<>();
+        for (JuegoEntity j : juegosEntity){
+            juegos.add(modelMapper.map(j,Juego.class));
+        }
+        return juegos;
     }
-    private List<Juego> buscarJuegoDePrecio(float RatingMax, float RatingMin){
-        //todo devolver una lista de juegos de un precio entre valores especificos
-        return null;
+    private List<Juego> buscarJuegoDePrecio(float PrecioMax, float PrecioMin){
+        //devolver una lista de juegos de un precio entre valores especificos
+        List<JuegoEntity> juegosEntity = juegoRepository.findByPrecio(PrecioMax,PrecioMin);
+        List<Juego> juegos = new ArrayList<>();
+        for (JuegoEntity j : juegosEntity){
+            juegos.add(modelMapper.map(j,Juego.class));
+        }
+        return juegos;
     }
     private List<Juego> buscarJuegoDeNombre(String nombre){
-        //todo devolver una lista de juegos siempre que el nombre pasado por parametro tenga coincidencias de letras o palabras con los juegos en la base de datos
-        return null;
+        //devolver una lista de juegos siempre que el nombre pasado por parametro tenga coincidencias de letras o palabras con los juegos en la base de datos
+        List<JuegoEntity> juegosEntity = juegoRepository.findByNombre(nombre);
+        List<Juego> juegos = new ArrayList<>();
+        for (JuegoEntity j : juegosEntity){
+            juegos.add(modelMapper.map(j,Juego.class));
+        }
+        return juegos;
     }
     private List<Juego> todosLosJuegos(){
         //todo trae todos los juegos y lo devuleve como lista
-        return null;
+        List<JuegoEntity> juegosEntity = juegoRepository.findAll();
+        List<Juego> juegos = new ArrayList<>();
+        for (JuegoEntity j : juegosEntity){
+            juegos.add(modelMapper.map(j,Juego.class));
+        }
+        return juegos;
     }
 
     /**
